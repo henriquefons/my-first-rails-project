0e293fa95ce6262c38fe8e3544261e55fc340d77
Henrique Fonseca <henriquefons@hotmail.com>
2021-06-14 12:14:34 -0300

    feat: integracao com o device

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
 
-    http_basic_authenticate_with name: "admin", password: "admin", 
-  except: [:index, :show]
+  before_action :authenticate_user! 
 
   def index
     @articles = Article.all
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "admin", password: "admin", 
-  only: :destroy
-
+  before_action :authenticate_user!
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
