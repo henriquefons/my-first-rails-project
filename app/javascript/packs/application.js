017648fec8fce5e2a5cc1e12fd420871e9cc884c
Henrique Fonseca <henriquefons@hotmail.com>
2021-06-15 08:49:09 -0300

    feat: bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'bootstrap';
+import "../stylesheets/application"
 import "channels"
 
 Rails.start()
